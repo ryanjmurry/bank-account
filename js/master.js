ad62659b7fae81f2913966f74de1c49e03cdec4f
Ryan Murry <ryanjmurry@gmail.com>
2018-03-20 00:30:20 -0700

    reset all forms after inputs.

--- js/master.js
+++ js/master.js
@@ -11,6 +11,8 @@ Account.prototype.Adjust = function () {
 function resetFields() {
   $("#new-deposit").val(0);
   $("#new-withdrawal").val(0);
+  $("#new-name").val("");
+  $("#initial-deposit").val(0);
 };
 
 //UI Logic
@@ -21,7 +23,7 @@ $(document).ready(function() {
     var inputtedName = $("#new-name").val();
     var initialDeposit = parseInt($("#initial-deposit").val());
     var userAccount = new Account(inputtedName, initialDeposit);
-
+    resetFields();
     $("form#account-update").submit(function() {
       event.preventDefault();
       debugger;
